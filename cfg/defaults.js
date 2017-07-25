594727f13a8f3c5e3d3995c0aaaeefd3185bf716
lovelife <1394798608@qq.com>
2017-07-25 20:07:47 +0800

    test3 dist

--- cfg/defaults.js
+++ cfg/defaults.js
@@ -62,7 +62,7 @@ function getDefaultModules() {
 
 module.exports = {
   srcPath: srcPath,
-  publicPath: 'assets/',
+  publicPath: 'gallery-by-react/assets/',
   port: dfltPort,
   getDefaultModules: getDefaultModules
 };
